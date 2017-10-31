0396f5371a7641b2feab6e2aa1e47cf954c6653e
figgyfunk <figa@rpi.edu>
2017-10-31 15:34:16 -0400

    particles now increase with pressing space bar which can be changed to whatever we want to change the particles.

--- Assets/changingParticles.cs
+++ Assets/changingParticles.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class changingParticles : MonoBehaviour {
-
+	int count = 0; 
+	List<GameObject> systems = new List<GameObject>();
+	public 
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,26 @@ public class changingParticles : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		int prevCount = count;
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			count += 1; 
+		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			count -= 1;		
+		}
+		if (count > prevCount) {
+
+			GameObject ps = (GameObject)Instantiate(Resources.Load("Particle System"));
+			ps.transform.position = new Vector3 (this.transform.position.x + Random.Range(.25f,.75f), this.transform.position.y +Random.Range(.25f,.75f), this.transform.position.z);
+			systems.Add (ps);
+		}
+		if (count < prevCount && systems.Count >0) {
+			int i = systems.Count; 
+			GameObject ps = systems [i - 1]; 
+			systems.Remove (ps);
+			Destroy (ps);
+	
+		}
 	}
+		
 }
