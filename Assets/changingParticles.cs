3d98455c495e53f09f9ae217513a49785e895674
figgyfunk <figa@rpi.edu>
2017-11-01 16:01:35 -0400

    frequency changes with distance to player.

--- Assets/changingParticles.cs
+++ Assets/changingParticles.cs
@@ -3,36 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class changingParticles : MonoBehaviour {
-	int count = 0; 
+
 	List<GameObject> systems = new List<GameObject>();
 	List<GameObject> woofs = new List<GameObject>();
 	public GameObject player;
-	public 
+	public GameObject dogs;
+	float prevDist;
+	float currentDist;
+	int changed = 0;
 	// Use this for initialization
 	void Start () {
 		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		int prevCount = count;
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			count += 1; 
-		}
-		if (Input.GetKeyDown (KeyCode.E)) {
-			count -= 1;		
-		}
-		if (count > prevCount) {
+		currentDist = Vector3.Distance (player.transform.position, dogs.transform.position);
 
+
+		if (currentDist < prevDist) {
+			//Debug.Log ("here");
 			GameObject ps = (GameObject)Instantiate(Resources.Load("Particle System"));
 			ps.transform.position = new Vector3 (this.transform.position.x , this.transform.position.y, this.transform.position.z+2f);
 			systems.Add (ps);
-			GameObject woof = (GameObject)Instantiate (Resources.Load ("woof"));
-			woof.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
-			woofs.Add (woof);
+			changed += 1;
+			if (changed == 5) {
+				GameObject woof = (GameObject)Instantiate (Resources.Load ("woof"));
+				woof.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z);
+				woofs.Add (woof);
+				changed = 0;
+			}
+
+
+
 		}
-		if (count < prevCount && systems.Count >0) {
+		/*if (currentDist > prevDist && systems.Count > 0) {
+			Debug.Log ("here2");
 			int i = systems.Count; 
 			GameObject ps = systems [i - 1]; 
 			systems.Remove (ps);
@@ -42,7 +50,7 @@ public class changingParticles : MonoBehaviour {
 			woofs.Remove (woof);
 			Destroy (woof);
 	
-		}
+		}*/
 
 		for (int i = 0; i < systems.Count; i++) {
 			systems[i].transform.position = new Vector3(player.transform.position.x+Random.Range(-5f,5f), player.transform.position.y, player.transform.position.z-1.6f);
@@ -50,6 +58,7 @@ public class changingParticles : MonoBehaviour {
 		for (int i = 0; i < woofs.Count; i++) {
 			woofs [i].transform.position = new Vector3 (player.transform.position.x +Random.Range(-5f,5f), player.transform.position.y +Random.Range(-3f,3f), player.transform.position.z-1.6f);
 		}
+		prevDist = currentDist;
 	}
 		
 }
