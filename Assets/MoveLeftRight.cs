d54d489a2b1b9d108a7ee7f344c3a7486b818c49
Matthew <chuangmatthew@yahoo.com>
2017-11-01 14:27:19 -0400

    add initial horizontal and vertical moving obstacles

---
+++ Assets/MoveLeftRight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftRight : MonoBehaviour {
+
+    public float xSpeed = 10;
+    public float ySpeed = -5;
+
+    public float lBound = 10;
+    public float rBound = 30;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.x <= lBound && xSpeed < 0)
+        {
+            xSpeed *= -1;
+        }
+        else if (gameObject.transform.position.x >= rBound && xSpeed > 0)
+        {
+            xSpeed *= -1;
+        }
+        transform.position = new Vector3(transform.position.x + xSpeed * Time.deltaTime, transform.position.y + ySpeed * Time.deltaTime, transform.position.z);
+    }
+}
