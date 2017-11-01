d1f56c212680e794aa1d965316882179867f92f3
figgyfunk <figa@rpi.edu>
2017-11-01 14:01:07 -0400

    dogs/barking moves at different times/give variety.

--- Assets/changingParticles.cs
+++ Assets/changingParticles.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class changingParticles : MonoBehaviour {
 	int count = 0; 
 	List<GameObject> systems = new List<GameObject>();
+	List<GameObject> woofs = new List<GameObject>();
 	public 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,7 @@ public class changingParticles : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		int prevCount = count;
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			count += 1; 
@@ -25,12 +27,19 @@ public class changingParticles : MonoBehaviour {
 			GameObject ps = (GameObject)Instantiate(Resources.Load("Particle System"));
 			ps.transform.position = new Vector3 (this.transform.position.x + Random.Range(.25f,.75f), this.transform.position.y +Random.Range(.25f,.75f), this.transform.position.z);
 			systems.Add (ps);
+			GameObject woof = (GameObject)Instantiate (Resources.Load ("woof"));
+			woof.transform.position = new Vector3(this.transform.position.x+Random.Range(-15f,15f), this.transform.position.y-4f, this.transform.position.z+2f);
+			woofs.Add (woof);
 		}
 		if (count < prevCount && systems.Count >0) {
 			int i = systems.Count; 
 			GameObject ps = systems [i - 1]; 
 			systems.Remove (ps);
 			Destroy (ps);
+			i = woofs.Count; 
+			GameObject woof = woofs [i - 1];
+			woofs.Remove (woof);
+			Destroy (woof);
 	
 		}
 	}
---
+++ Assets/woofSpawn.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woofSpawn : MonoBehaviour {
+	bool fadeIn;
+	float targetTime;
+	float spawnTime;
+	// Use this for initialization
+	void Start () {
+		fadeIn = true;
+		targetTime = Random.Range(0f, 3f);
+		spawnTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (Time.time - spawnTime >= targetTime) {
+			if (fadeIn) {
+				gameObject.GetComponent<Renderer> ().material.color = new Color (gameObject.GetComponent<Renderer> ().material.color.r, gameObject.GetComponent<Renderer> ().material.color.g, gameObject.GetComponent<Renderer> ().material.color.b, gameObject.GetComponent<Renderer> ().material.color.a + .01f);
+				gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y+.2f , this.transform.position.z);
+				if (gameObject.GetComponent<Renderer>().material.color.a >= 1f) {
+					fadeIn = false;
+				}
+			} 
+			else {
+				gameObject.GetComponent<Renderer>().material.color = new Color (gameObject.GetComponent<Renderer> ().material.color.r, gameObject.GetComponent<Renderer> ().material.color.g, gameObject.GetComponent<Renderer> ().material.color.b, gameObject.GetComponent<Renderer> ().material.color.a - .01f); 
+				gameObject.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y-.2f, this.transform.position.z);
+				if (gameObject.GetComponent<Renderer>().material.color.a <= 0f) {
+					fadeIn = true;
+				}
+			}
+		}
+
+	}
+}
