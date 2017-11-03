7c98630738f4481ec19ff078f3770081931ae2ee
figgyfunk <figa@rpi.edu>
2017-11-02 21:33:17 -0400

    working invisibility power up.

--- Assets/powerUp.cs
+++ Assets/powerUp.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class powerUp : MonoBehaviour {
-
+	GameObject [] obstacles;
+	bool power = false;
+	float powerTimer = 0;
+	Collider player;
 	// Use this for initialization
 	void Start () {
-		
+		obstacles = GameObject.FindGameObjectsWithTag("obstacle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+
+		if (power && Time.time -powerTimer <= 15f) {
+			Debug.Log ("POWER");
+			foreach (GameObject obstacle in obstacles) {
+				Physics.IgnoreCollision(obstacle.GetComponent<Collider>(), player.GetComponent<Collider>());
+			}
+		}
+		if (Time.time - powerTimer > 15f) {
+			power = false; 
+			foreach (GameObject obstacle in obstacles) {
+				Physics.IgnoreCollision(obstacle.GetComponent<Collider>(), player.GetComponent<Collider>(),false);
+			}
+			powerTimer = 0f;
+
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		power = true;
+		player = other;
+		powerTimer = Time.time;
 	}
 }
