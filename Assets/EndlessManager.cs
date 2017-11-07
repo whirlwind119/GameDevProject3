baba7fc422456069e1be9474232cdabb455281a2
Tim Kim <tim.kim1996@gmail.com>
2017-11-07 18:32:14 -0500

    Added Endless mode

---
+++ Assets/EndlessManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessManager : MonoBehaviour {
+
+    public GameObject level1;
+    public GameObject level2;
+    public GameObject level3;
+    public GameObject level4;
+    public GameObject level5;
+
+    private int random;
+    private GameObject current;
+    private GameObject next;
+    private GameObject player;
+    private int counter;
+    private int counter2;
+    private List<GameObject> path = new List<GameObject>();
+
+    // Use this for initialization
+    void Start () {
+        counter = 1;
+        counter2 = 0;
+        player = GameObject.FindGameObjectWithTag("Player");
+        random = Random.Range(1, 5);
+        current = Instantiate(pickLevel(random), new Vector3(0f, 0f, 0f), Quaternion.identity);
+        path.Add(current);
+        random = Random.Range(1, 5);
+        next = pickLevel(random);
+        if (next == current) {
+            selectNext(random);
+        }
+        next = Instantiate(next, new Vector3(0f, 0f, 172.2f * counter), Quaternion.identity);
+        path.Add(next);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.transform.position.z > 86.1f + 172.2*counter2) {
+            counter++;
+            counter2++;
+            current = next;
+            random = Random.Range(1, 5);
+            next = pickLevel(random);
+            if (next == current) {
+                selectNext(random);
+            }
+            
+            GameObject temp = current;
+            next = Instantiate(next, new Vector3(0f, 0f, 172.2f * counter), Quaternion.identity);
+            path.Add(next);
+            temp = path[0];
+            path.Remove(path[0]);
+            Destroy(temp);
+        }
+    }
+
+    private GameObject pickLevel(int randomInt) {
+        GameObject to_return = null;
+        if (randomInt == 1) {
+            to_return = level1;
+        }
+        if (randomInt == 2) {
+            to_return = level2;
+        }
+        if (randomInt == 3) {
+            to_return = level3;
+        }
+        if (randomInt == 4) {
+            to_return = level4;
+        }
+        if (randomInt == 5) {
+            to_return = level5;
+        }
+        return to_return;
+    }
+
+    private GameObject selectNext(int randomInt) {
+        GameObject to_return = null;
+        if (randomInt == 1) {
+            to_return = level2;
+        }
+        if (randomInt == 2) {
+            to_return = level3;
+        }
+        if (randomInt == 3) {
+            to_return = level4;
+        }
+        if (randomInt == 4) {
+            to_return = level5;
+        }
+        if (randomInt == 5) {
+            to_return = level1;
+        }
+        return to_return;
+    }
+}
