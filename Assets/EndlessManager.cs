f7ebac1bc4e41022ea2e5ba8c6d821794e5b3885
whirlwind119 <tim.kim1996@gmail.com>
2017-11-08 12:49:28 -0500

    made sure the walls also spawn in endless

--- Assets/EndlessManager.cs
+++ Assets/EndlessManager.cs
@@ -12,6 +12,10 @@ public class EndlessManager : MonoBehaviour {
     public GameObject level6;
     public GameObject level7;
     public GameObject level8;
+    public GameObject leftWall;
+    public GameObject rightWall;
+    public GameObject temp1;
+    public GameObject temp2;
 
     private int random;
     private GameObject current;
@@ -23,6 +27,7 @@ public class EndlessManager : MonoBehaviour {
     private float fakeGroundDistance;
     private int counter;
     private int counter2;
+    private int wallCounter = 1;
     private List<GameObject> path = new List<GameObject>();
 
     // Use this for initialization
@@ -51,6 +56,15 @@ public class EndlessManager : MonoBehaviour {
         background.transform.position = (background.transform.position - player.transform.position).normalized * backgroundDistance + player.transform.position;
         floor.transform.position = (floor.transform.position - player.transform.position).normalized * fakeGroundDistance + player.transform.position;
         if (player.transform.position.z > 86.1f + 172.2*counter2) {
+            
+            if (counter2 % 1 == 0) {
+                wallCounter+=2;
+                temp1 = Instantiate(leftWall, new Vector3(-13.86f, 15.8f, 170f + 463.6f * wallCounter), Quaternion.identity);
+                temp2 = Instantiate(rightWall, new Vector3(13.86f, 15.8f, 463.6f * wallCounter), Quaternion.identity);
+                temp1.transform.eulerAngles = new Vector3(0f, 180f, 90f);
+                temp2.transform.eulerAngles = new Vector3(0f, 0f, 90f);
+            }
+            
             counter++;
             counter2++;
             current = next;
