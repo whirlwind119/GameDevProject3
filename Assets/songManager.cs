329b62292801977142ea128102d72fd6204c869f
figgyfunk <figa@rpi.edu>
2017-11-08 04:03:47 -0500

    level2 and level3 done.

--- Assets/songManager.cs
+++ Assets/songManager.cs
@@ -7,10 +7,14 @@ public class songManager : MonoBehaviour {
 
     public static string currentScene = "";
     public static string previousScene = "";
+	bool setSong;
+	bool setSong2;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("TESTING DEBUG LOG");
+        //Debug.Log("TESTING DEBUG LOG");
+		setSong = true;
+		setSong2 = false;
     }
 	
 	// Update is called once per frame
@@ -19,9 +23,29 @@ public class songManager : MonoBehaviour {
         {
             previousScene = currentScene;
             currentScene = SceneManager.GetActiveScene().name;
-            Debug.Log("Current Scene: " + currentScene);
-            Debug.Log("Previous Scene: " + previousScene);
+            //Debug.Log("Current Scene: " + currentScene);
+            //Debug.Log("Previous Scene: " + previousScene);
         }
+
+		if (setSong && currentScene == "win") {
+			AudioSource a = this.gameObject.GetComponent<AudioSource> ();
+			a.clip = Resources.Load ("winTheme") as AudioClip;
+			a.loop = true;
+			a.volume = .5f;
+			a.PlayDelayed (0f);
+			setSong = false;
+			setSong2 = true;
+		}
+		if (setSong2 && currentScene == "Start Menu") {
+			AudioSource a = this.gameObject.GetComponent<AudioSource> (); 
+			a.clip = Resources.Load ("skate") as AudioClip;
+			a.loop = true; 
+			a.volume = .5f; 
+			a.PlayDelayed (0f); 
+			setSong2 = false; 
+			setSong = true;
+		}
+
 	}
 
 	void Awake(){
